954d41458395264f5b32bc4f7414d29b35d34328
FeToR <fedornigretuk@gmail.com>
2022-09-25 00:26:02 +0300

    now works with asyncio

--- QueueBot.py
+++ QueueBot.py
@@ -1,9 +1,14 @@
 import telegram
 import toml
+import asyncio
 
 with open("config.toml") as f:
     config = toml.load(f)
 
 
-bot = telegram.Bot(token=config["token"])
-print(bot.get_me())
+async def main():
+    bot = telegram.Bot(token=config["token"])
+    print(await bot.get_me())
+
+
+asyncio.get_event_loop().run_until_complete(main())
