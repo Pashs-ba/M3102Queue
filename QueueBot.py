d36726b3e4b658d64d4ea9d589fa6c97372ff1ae
Pashs-ba <pashs.ba@gmail.com>
2022-09-24 22:09:01 +0300

    make config

--- QueueBot.py
+++ QueueBot.py
@@ -1,12 +1,15 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import toml
 
+with open("config.toml") as f:
+    config = toml.load(f)
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token("1339453682:AAH-D0KZ49an5Y4JFMHYSo9ss_TloD2XV3I").build()
+app = ApplicationBuilder().token(config["common"]["token"]).build()
 
 app.add_handler(CommandHandler("hello", hello))
 
