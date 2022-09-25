6677d2fe6d4c4bcd956edd65e8914636abc89280
Pashs-ba <pashs.ba@gmail.com>
2022-09-25 09:30:09 +0300

    fix token

--- QueueBot.py
+++ QueueBot.py
@@ -9,7 +9,7 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token(config["common"]["token"]).build()
+app = ApplicationBuilder().token(config["token"]).build()
 
 app.add_handler(CommandHandler("hello", hello))
 
