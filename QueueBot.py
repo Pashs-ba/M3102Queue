646c44b4dee531ee64db2fb7eb8733b478fc3e5d
FeToR <fedornigretuk@gmail.com>
2022-09-30 00:42:24 +0300

    added create, add, remove, show, delete, active

--- QueueBot.py
+++ QueueBot.py
@@ -1,70 +1,90 @@
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, Updater
+from telegram.ext import Application, CommandHandler
 import toml
-from uuid import uuid4
 
 with open("config.toml") as f:
     config = toml.load(f)
 
 
-async def a(update, context):
-    key = context.args[0]
-    if key in context.user_data:
-
-        user = update.effective_user
-        us = f"{user['first_name']} {user['last_name']}"
-
-        if us not in context.user_data[key]:
-            context.user_data[key] += [us]
-
-        await update.message.reply_text(f"You're the {context.user_data[key].index(us)}-th")
-    else:
+async def check_queue(update, context, key):
+    if key not in context.user_data:
         await update.message.reply_text(f"Queue {key} not found")
+        return False
+    return True
 
 
-async def r(update, context):
-    key = context.args[0]
-    if key in context.user_data:
+async def add_a_person(update, context):
+    key = update.message.text.partition(' ')[2] or "queue"
+    if not await check_queue(update, context, key):
+        return
 
-        user = update.effective_user
-        us = f"{user['first_name']} {user['last_name']}"
+    user = update.effective_user
 
-        if us in context.user_data[key]:
-            context.user_data[key] -= [us]
-        await update.message.reply_text(f"Removed")
+    if user not in context.user_data[key]:
+        context.user_data[key] += [user]
 
-    else:
-        await update.message.reply_text(f"Queue {key} not found")
+    await update.message.reply_text(f"You're the {context.user_data[key].index(user) + 1}-th")
 
 
-async def show(update, context):
-    pass
+async def remove_a_person(update, context):
+    key = update.message.text.partition(' ')[2] or "queue"
+    if not await check_queue(update, context, key):
+        return
+
+    user = update.effective_user
+
+    if user in context.user_data[key]:
+        context.user_data[key].remove(user)
+    await update.message.reply_text(f"Done")
 
 
-async def AddQueue(update, context) -> None:
-    key = context.args[0]
+async def show_queue(update, context):
+    key = update.message.text.partition(' ')[2] or "queue"
+    if not await check_queue(update, context, key):
+        return
+
+    queue = ""
+    for i, j in enumerate(context.user_data[key]):
+        k = f"{j['first_name']} {j['last_name']}"
+        queue += f"{i+1}. {k}\n"
+
+    await update.message.reply_text(queue or "Empty")
+
+
+async def add_queue(update, context):
+    key = update.message.text.partition(' ')[2] or "queue"
+    if await check_queue(update, context, key):
+        return
+
     context.user_data[key] = []
-
-    await update.message.reply_html(rf"Queue {key} created!")
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(update.message.text)
+    await update.message.reply_html(f"Queue {key} created!")
 
 
-def main() -> None:
+async def delete_queue(update, context):
+    key = update.message.text.partition(' ')[2] or "queue"
+    if not await check_queue(update, context, key):
+        return
+    del context.user_data[key]
+    await update.message.reply_html(f"Queue {key} deleted!")
+
+
+async def show_active(update, context):
+    queues = ""
+    for i, j in enumerate(context.user_data):
+        queues += f"{i}. {j}\n"
+    await update.message.reply_html(queues or "Empty")
+
+
+def main():
     application = Application.builder().token(config['token']).build()
 
-    application.add_handler(CommandHandler("create", AddQueue))
-    application.add_handler(CommandHandler("a", a))
-    application.add_handler(CommandHandler("r", r))
-
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(CommandHandler("create", add_queue))
+    application.add_handler(CommandHandler("add", add_a_person))
+    application.add_handler(CommandHandler("remove", remove_a_person))
+    application.add_handler(CommandHandler("show", show_queue))
+    application.add_handler(CommandHandler("delete", delete_queue))
+    application.add_handler(CommandHandler("active", show_active))
 
     application.run_polling()
-
-
-#    updater.idle()
 
 
 if __name__ == "__main__":
